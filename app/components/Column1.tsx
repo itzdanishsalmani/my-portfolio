3fab7b940567c9bcaf0cae810629558b8db132b4
danishsalmani <salmanidanish488@gmail.com>
2024-07-17 12:00:34 +0530

    resume url updated

--- app/components/Column1.tsx
+++ app/components/Column1.tsx
@@ -1,37 +1,39 @@
 import { Transition } from "./Units"
 import Image from "next/image"
 
-export const Col1 = () =>{
+export const Col1 = () => {
     return (
         <div className="font-mono">
-        <div className="md:ml-32 md:fixed ml-4 mt-24 text-white">
-        <div className="md:text-4xl text-2xl font-bold">
-        MOHAMMED DANISH
-    </div>
-    <div className="mt-4 ">
-        Full Stack Developer
-    </div>
-
-    <div className="mt-4 text-slate-400 ">
-        I build full stack application
-    </div>
-
-    <div className="mt-24 text-slate-500 flex flex-col items-start">
-
-        <Transition input="ABOUT" id="#about" />
-        <Transition input="SKILLS" id="#skills" />
-        <Transition input="PROJECTS" id="#projects" />
-    
-    </div>
-
-        <div className="mt-24 flex space-x-4">
-            
-            <a href="https://github.com/itzdanishsalmani" target="_blank"> <Image src="/github.png" width={100} height={50}  alt=""  className="  w-8 rounded-full" /></a>
-            <a href="https://www.linkedin.com/in/mohammed-danish-salmani-ab2586258/" target="_blank"> <Image src="/linkedin.png" width={100} height={50}  alt="" className=" w-8 rounded-lg" /></a>
-            <a href="https://x.com/itzzdanish" target="_blank"> <Image src="/twitter.png" width={100} height={50}  alt="" className="  w-8 rounded-lg" /></a>
-            <a href="https://mailto:salmanidanish488@gmail.com" target="_blank"> <Image src="/mail.png" width={100} height={50}  alt="" className="  w-8 rounded-lg" /></a>
+            <div className="md:ml-32 md:fixed ml-4 mt-24 text-white">
+                <div className="md:text-4xl text-2xl font-bold">
+                    MOHAMMED DANISH
+                </div>
+                <div className="mt-4">
+                    Full Stack Developer
+                </div>
+                <div className="mt-4 text-slate-400">
+                    I build full stack applications
+                </div>
+                <div className="mt-24 text-slate-500 flex flex-col items-start">
+                    <Transition input="ABOUT" id="#about" />
+                    <Transition input="SKILLS" id="#skills" />
+                    <Transition input="PROJECTS" id="#projects" />
+                </div>
+                <div className="mt-24 flex space-x-4">
+                    <a href="https://github.com/itzdanishsalmani" target="_blank">
+                        <Image src="/github.png" width={25} height={25} alt="GitHub" className="rounded-full" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/mohammed-danish-salmani-ab2586258/" target="_blank">
+                        <Image src="/linkedin.png" width={25} height={25} alt="LinkedIn" className="rounded-lg" />
+                    </a>
+                    <a href="https://x.com/itzzdanish" target="_blank">
+                        <Image src="/twitter.png" width={25} height={25} alt="Twitter" className="rounded-lg" />
+                    </a>
+                    <a href="mailto:salmanidanish488@gmail.com" target="_blank">
+                        <Image src="/mail.png" width={25} height={25} alt="Email" className="rounded-lg" />
+                    </a>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-    )
-}
+    );
+};
--- app/components/Column2.tsx
+++ app/components/Column2.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import { ProjectCard } from "./Units"
+import React from "react";
+import { ProjectCard } from "./Units";
 import { Span } from "./Units";
 import Image from "next/image";
 
 export const Col2 = () => {
     const buttons1 = ["React", "Express", "MongoDB Atlas", "Node.js", "JavaScript", "Tailwind CSS", "Vercel", "PayPal API", "Vercel"];
-    const buttons2 = ["TypeScript", "React", "Hono", "Postgresql", "Prisma ORM", "Tailwind CSS","Vercel", "Cloudflare workers" ]
-    const buttons3 = ["React", "Express", "MongoDB Atlas", "Node.js", "JavaScript", "Tailwind CSS", "Vercel"]
+    const buttons2 = ["TypeScript", "React", "Hono", "Postgresql", "Prisma ORM", "Tailwind CSS","Vercel", "Cloudflare workers"];
+    const buttons3 = ["React", "Express", "MongoDB Atlas", "Node.js", "JavaScript", "Tailwind CSS", "Vercel"];
     
     interface Icon {
-        src:string;
-        alt:string;
+        src: string;
+        alt: string;
     }
     const icons: Icon[] = [
         { src: "html-5.svg", alt: "HTML5" },
@@ -32,56 +32,54 @@ export const Col2 = () => {
     return (
         <div className="font-mono mx-4 md:mr-36 ">
             <div id="about" className="mt-24 text-slate-400">
-                I'm a passionate and dedicated developer. My expertise lies in the <Span input="MERN stack"/>, <Span input=" TypeScript"/>, and <Span input="PostgreSQL" />. With a strong foundation in these technologies, I have successfully built three full-stack projects, showcasing my ability to develop robust and scalable web applications.
+                I&apos;m a passionate and dedicated developer. My expertise lies in the <Span input="MERN stack"/>, <Span input="TypeScript"/>, and <Span input="PostgreSQL" />. With a strong foundation in these technologies, I have successfully built three full-stack projects, showcasing my ability to develop robust and scalable web applications.
                 <br /><br />
                 I thrive on solving complex problems and continuously improving my skills. My journey in web development has equipped me with the knowledge and experience to create efficient and user-friendly solutions. I am always eager to learn new technologies and take on challenging projects.
                 <br /><br />
-                Whether it's developing dynamic front-end interfaces or designing efficient back-end architectures, I bring a meticulous and innovative approach to my work. I am committed to delivering high-quality code and collaborating effectively with teams to achieve project goals.
+                Whether it&apos;s developing dynamic front-end interfaces or designing efficient back-end architectures, I bring a meticulous and innovative approach to my work. I am committed to delivering high-quality code and collaborating effectively with teams to achieve project goals.
             </div>
 
             <div id="skills" className="mt-24 p-4 rounded-lg hover:bg-slate-800">
-            <div className="grid grid-cols-5 items-center gap-4 cursor-pointer">
-            {icons.map((icon, index) => (
-                    <img key={index} className="w-12 h-20" src={icon.src} alt={icon.alt} />
-                ))}
-              </div>
+                <div className="grid grid-cols-5 items-center gap-4 cursor-pointer">
+                    {icons.map((icon, index) => (
+                        <Image key={index} className="w-12 h-20" src={icon.src} alt={icon.alt} width={48} height={80} />
+                    ))}
+                </div>
             </div>
 
             <div className="mt-24 cursor-pointer text-white hover:text-emerald-600">
-                <a href="https://drive.google.com/file/d/1uACrjuC5Ui_ZSM2-tITuivJUK2Ifvx_1/view" target="_blank"> View Full Resume </a>
+                <a href="https://drive.google.com/file/d/1GUlB5YCQHwWWmlMQgsGADDDoSiM_28Nx/view?usp=sharing" target="_blank"> View Full Resume </a>
             </div>
 
             <div id="projects" className="mt-24">
-            <ProjectCard
-                liveLink="https://hyperdev.vercel.app"
-                image="/hyperdev.png"
-                title="HyperDev - Course selling web application"
-                description="It is a Full Stack responsive web application in MERN Stack with PayPal Payment Gateway, where Admin can
-                perform CRUD operation and user can pay and buy courses."
-                buttons={buttons1}
-            />
+                <ProjectCard
+                    liveLink="https://hyperdev.vercel.app"
+                    image="/hyperdev.png"
+                    title="HyperDev - Course selling web application"
+                    description="It is a Full Stack responsive web application in MERN Stack with PayPal Payment Gateway, where Admin can perform CRUD operation and user can pay and buy courses."
+                    buttons={buttons1}
+                />
                 
-            <ProjectCard
-                liveLink="https://mediumm.vercel.app"
-                image="/mediumm.png"
-                title="Mediumm - Blogging web application"
-                description="Develop a full-stack responsive web app where authenticated users can create and update blog posts with descriptions."
-                buttons={buttons2}
-            />
+                <ProjectCard
+                    liveLink="https://mediumm.vercel.app"
+                    image="/mediumm.png"
+                    title="Mediumm - Blogging web application"
+                    description="Develop a full-stack responsive web app where authenticated users can create and update blog posts with descriptions."
+                    buttons={buttons2}
+                />
                 
-            <ProjectCard
-                liveLink="https://payze.vercel.app"
-                image="/payze.png"
-                title="Payze - Online payment web application"
-                description="It is an online payment web application in MERN Stack
-                where users can send payment to each other with virtual money."
-                buttons={buttons3}
-            />
-
+                <ProjectCard
+                    liveLink="https://payze.vercel.app"
+                    image="/payze.png"
+                    title="Payze - Online payment web application"
+                    description="It is an online payment web application in MERN Stack where users can send payment to each other with virtual money."
+                    buttons={buttons3}
+                />
             </div>
 
             <div className="my-24 text-slate-400 text-sm">
-            Coded in <a href="https://code.visualstudio.com/"><Span input="Visual Studio Code"/></a> by yours truly. Built with <a href="https://nextjs.org/"><Span input="Next.js"/></a> and <a href="https://tailwindcss.com/"><Span input="Tailwind CSS"/></a>, deployed with <a href="https://vercel.com/"><span className="text-slate-300">Vercel.</span></a>         </div>
+                Coded in <a href="https://code.visualstudio.com/"><Span input="Visual Studio Code"/></a> by yours truly. Built with <a href="https://nextjs.org/"><Span input="Next.js"/></a> and <a href="https://tailwindcss.com/"><Span input="Tailwind CSS"/></a>, deployed with <a href="https://vercel.com/"><span className="text-slate-300">Vercel.</span></a>
+            </div>
         </div>
-    )
-}
+    );
+};
