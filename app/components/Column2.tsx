0607957e997dd5e056d79f367d32db60d0ef4315
Danish <salmanidanish488@gmail.com>
2025-04-08 19:59:08 +0530

    [Updated]: Resume URL

--- app/components/Column2.tsx
+++ app/components/Column2.tsx
@@ -36,7 +36,7 @@ export const Col2 = () => {
     return (
         <div className="font-mono mx-4 md:mr-36 ">
             <div id="about" className="mt-24 text-slate-400">
-                I&apos;m a passionate and dedicated developer. My expertise lies in the <Span input="MERN stack"/>, <Span input="TypeScript"/>, and <Span input="React native" />. With a strong foundation in these technologies, I have successfully built several full-stack projects, showcasing my ability to develop robust and scalable web and mobile applications.
+                I&apos;m a passionate and dedicated developer. My expertise lies in the <Span input="MERN stack"/>, <Span input="TypeScript"/>, and <Span input="JavaScript" />. With a strong foundation in these technologies, I have successfully built several full-stack projects, showcasing my ability to develop robust and scalable web applications.
                 <br /><br />
                 I thrive on solving complex problems and continuously improving my skills. My journey in web development has equipped me with the knowledge and experience to create efficient and user-friendly solutions. I am always eager to learn new technologies and take on challenging projects.
                 <br /><br />
@@ -53,7 +53,7 @@ export const Col2 = () => {
 
             <div className="mt-24 flex space-x-2 cursor-pointer text-white hover:text-emerald-600">
                 <div>
-                <a href="https://drive.google.com/file/d/1pAsjfdJXZGJfqBzZJD-jLQlyi6J7U99M/view?usp=sharing" target="_blank"> View Full Resume </a></div>
+                <a href="https://drive.google.com/file/d/1oMw4uC00EdD7rXC4wzLOfvyCuCSxBgTg/view?usp=sharing" target="_blank"> View Full Resume </a></div>
                 <div> <Image src={"arrow_north_east.svg"} alt="arrow" height={20} width={20} /> </div>
             </div>
 
